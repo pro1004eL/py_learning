20a646bdc629f867a81d9e1bf6a9c9d9f029d4e1
anton k <anton.kolomiets.91@gmail.com>
2024-07-22 18:13:09 +0300

    tasks 8-13

--- main.py
+++ main.py
@@ -16,13 +16,35 @@ code_for = 'For'
 all = 'All'
 #print(f'{coding} {code_for} {all}')
 
-#todo day 4 #3
-company = "Coding for all"
-#print(company)
 
 #todo day 4 #5
 #print(len(company), '= lens of the "company" var')
 
 #todo day 4 #6, 7
-print(company.upper() +' = upper and lower = '+ company.lower())
+#print(company.upper() +' = upper and lower = '+ company.lower())
 
+#todo day 4 #8
+#capital = company.capitalize()
+# title = company.title()
+# swapca = company.swapcase()
+# print(capital +' '+ title +' '+ swapca)
+
+#todo day 4 #9
+#sli = company[6:]
+#print(sli)
+
+#todo day 4 #10
+company = "Coding For All"
+contein = company.index('Coding')
+#print(contein)
+
+#todo day 4 #11
+re = company.replace('Coding', 'Python')
+#print(re)
+
+#todo day 4 #12
+new_re = 'Python for Everyone'
+print(new_re.replace('Everyone', 'All'))
+
+#todo day 4 #13
+print(company.split())
