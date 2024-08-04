515b3ecf4bdc6c55845ca73a29fd9bda7395da54
anton k <anton.kolomiets.91@gmail.com>
2024-08-04 17:59:29 +0300

    new Tuples list

---
+++ tuples.py
@@ -0,0 +1,9 @@
+print(' ')
+
+
+#todo Day 6
+family_members = ('Anton', 'Kostya', 'Katya', 'Tanya', 'Viktor')
+print(family_members)
+
+i_am, brother, sister, mama, father = family_members
+print(brother)
