62f5c00074ffaba849ce4bacb4ab1e216f0e3217
anton k <anton.kolomiets.91@gmail.com>
2024-08-04 18:11:02 +0300

    Tuples lvl 2 HW complete

--- tuples.py
+++ tuples.py
@@ -7,3 +7,11 @@ print(family_members)
 
 i_am, brother, sister, mama, father = family_members
 print(brother)
+
+mid_item = len(family_members) // 2
+print('Middle item: ', family_members[mid_item])
+
+first3 = family_members[0:3]
+last_three = family_members[-3:]
+print(first3, last_three)
+print("Kostya" and "Anton" in family_members)
