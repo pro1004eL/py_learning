9ae564bbfe5e303fd6574618ea0db23fe7463c7f
anton k <anton.kolomiets.91@gmail.com>
2024-08-08 18:55:28 +0300

    day#8, #3-5

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,32 @@
 
 print( ' ')
 
-#todo Day 8
+#todo Day 8 #1 , #2
 
+dog = {
+    'name': 'Barsik',
+    'color': 'Black',
+    'breed': 'Best_home',
+    'legs': 4,
+    'age': 16
+}
+print(dog)
 
+#todo #3, #4
+student = {
+    'first_name': 'Anton',
+    'last_name': 'BEgemot',
+    'gender': 'Male',
+    'age': '33',
+    'marital status': 'single',
+    'skills': ['SQL', 'HTML', 'CSS', 'Python', 'Testing'],
+    'country': 'Ukraine',
+    'city': 'Kharkiv',
+    'address': {
+        'street': 'Plehanovska, 43',
+        'zip_code': 11100
+    }
+}
+
+skill_values = student['skills']
+print(type(skill_values), '\nskills: ', skill_values)
